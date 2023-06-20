59b638a4e2f063c28f414c49ede21fdaa16eb911
Kostya <fedurovk@gmail.com>
2023-06-20 11:15:06 +0300

    tmp

--- fresh_tile/packages/diagnosticable_generator/lib/builder.dart
+++ fresh_tile/packages/diagnosticable_generator/lib/builder.dart
@@ -2,5 +2,6 @@ import 'package:build/build.dart';
 import 'package:diagnosticable_generator/diagnosticable_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder diagnosticableBuilder(BuilderOptions options) =>
-    SharedPartBuilder([DiagnosticableGenerator()], 'diagnosticable');
+Builder diagnosticableBuilder(BuilderOptions options) {
+  return SharedPartBuilder([DiagnosticableGenerator()], 'diagnosticable');
+}
